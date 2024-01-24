1002fe021937295da728f37e382d6667e0f1d00b
Bruno Evangelista <bpevangelista@gmail.com>
2024-01-23 18:02:56 -0800

    Added default rasterDesc and blendDesc

--- src/fastdx/fastdx.h
+++ src/fastdx/fastdx.h
@@ -26,11 +26,12 @@ namespace fastdx {
         }
     };
 
-    typedef std::shared_ptr<ID3D12Device2> ID3D12DevicePtr;
     typedef std::shared_ptr<ID3D12CommandAllocator> ID3D12CommandAllocatorPtr;
-    typedef std::shared_ptr <ID3D12GraphicsCommandList6> ID3D12GraphicsCommandListPtr;
-    typedef std::shared_ptr<ID3D12DescriptorHeap> ID3D12DescriptorHeapPtr;
     typedef std::shared_ptr<ID3D12CommandQueue> ID3D12CommandQueuePtr;
+    typedef std::shared_ptr<ID3D12DescriptorHeap> ID3D12DescriptorHeapPtr;
+    typedef std::shared_ptr<ID3D12Device2> ID3D12DevicePtr;
+    typedef std::shared_ptr<ID3D12Fence> ID3D12FencePtr;
+    typedef std::shared_ptr <ID3D12GraphicsCommandList6> ID3D12GraphicsCommandListPtr;
     typedef std::shared_ptr<IDXGISwapChain3> IDXGISwapChainPtr;
 
     class D3D12DeviceWrapper;
@@ -54,21 +55,23 @@ namespace fastdx {
         D3D12DeviceWrapper(ID3D12DevicePtr device) :
             _device(device) {}
 
-        ID3D12CommandQueuePtr createCommandQueue(D3D12_COMMAND_LIST_TYPE type, HRESULT* outResult = nullptr);
-
-        IDXGISwapChainPtr createWindowSwapChain(ID3D12CommandQueuePtr commandQueue, uint32_t bufferCount, DXGI_FORMAT format,
-            HRESULT* outResult = nullptr);
-        IDXGISwapChainPtr createWindowSwapChain(ID3D12CommandQueuePtr commandQueue,DXGI_SWAP_CHAIN_DESC1 swapChainDesc,
-            HWND hwnd, HRESULT* outResult = nullptr);
-
-        ID3D12DescriptorHeapPtr createHeapDescriptor(int32_t count, D3D12_DESCRIPTOR_HEAP_TYPE heapType,
-            HRESULT* outResult = nullptr);
+        ID3D12DevicePtr d3dDevice() const { return _device; }
 
         ID3D12CommandAllocatorPtr createCommandAllocator(D3D12_COMMAND_LIST_TYPE commandType,
             HRESULT* outResult = nullptr);
 
         ID3D12GraphicsCommandListPtr createCommandList(uint32_t nodeMask, D3D12_COMMAND_LIST_TYPE commandType,
             ID3D12CommandAllocatorPtr allocator, HRESULT* outResult = nullptr);
+
+        ID3D12CommandQueuePtr createCommandQueue(D3D12_COMMAND_LIST_TYPE type, HRESULT* outResult = nullptr);
+
+        ID3D12DescriptorHeapPtr createHeapDescriptor(int32_t count, D3D12_DESCRIPTOR_HEAP_TYPE heapType,
+            HRESULT* outResult = nullptr);
+
+        IDXGISwapChainPtr createWindowSwapChain(ID3D12CommandQueuePtr commandQueue, uint32_t bufferCount, DXGI_FORMAT format,
+            HRESULT* outResult = nullptr);
+        IDXGISwapChainPtr createWindowSwapChain(ID3D12CommandQueuePtr commandQueue, DXGI_SWAP_CHAIN_DESC1 swapChainDesc,
+            HWND hwnd, HRESULT* outResult = nullptr);
 
         void createRenderTargetViews(IDXGISwapChainPtr swapChain, ID3D12DescriptorHeapPtr heap,
             HRESULT* outResult = nullptr);
@@ -78,3 +81,46 @@ namespace fastdx {
     };
 };
 
+
+/// Defaults
+namespace fastdx {
+
+    struct DEFAULT_D3D12_RASTERIZER_DESC : public D3D12_RASTERIZER_DESC {
+        DEFAULT_D3D12_RASTERIZER_DESC() {
+            FillMode = D3D12_FILL_MODE_SOLID;
+            CullMode = D3D12_CULL_MODE_BACK;
+            FrontCounterClockwise = FALSE;
+            DepthBias = D3D12_DEFAULT_DEPTH_BIAS;
+            DepthBiasClamp = D3D12_DEFAULT_DEPTH_BIAS_CLAMP;
+            SlopeScaledDepthBias = D3D12_DEFAULT_SLOPE_SCALED_DEPTH_BIAS;
+            DepthClipEnable = TRUE;
+            MultisampleEnable = FALSE;
+            AntialiasedLineEnable = FALSE;
+            ForcedSampleCount = 0;
+            ConservativeRaster = D3D12_CONSERVATIVE_RASTERIZATION_MODE_OFF;
+        }
+    };
+    D3D12_RASTERIZER_DESC defaultRasterDesc() { return DEFAULT_D3D12_RASTERIZER_DESC(); }
+
+
+    struct DEFAULT_D3D12_BLEND_DESC : public D3D12_BLEND_DESC {
+        DEFAULT_D3D12_BLEND_DESC() {
+            AlphaToCoverageEnable = FALSE;
+            IndependentBlendEnable = FALSE;
+
+            for (int32_t i = 0; i < _countof(RenderTarget); ++i) {
+                RenderTarget[i].BlendEnable = FALSE;
+                RenderTarget[i].LogicOpEnable = FALSE;
+                RenderTarget[i].SrcBlend = D3D12_BLEND_ONE;
+                RenderTarget[i].DestBlend = D3D12_BLEND_ZERO;
+                RenderTarget[i].BlendOp = D3D12_BLEND_OP_ADD;
+                RenderTarget[i].SrcBlendAlpha = D3D12_BLEND_ONE;
+                RenderTarget[i].DestBlendAlpha = D3D12_BLEND_ZERO;
+                RenderTarget[i].BlendOpAlpha = D3D12_BLEND_OP_ADD;
+                RenderTarget[i].LogicOp = D3D12_LOGIC_OP_NOOP;
+                RenderTarget[i].RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;
+            }
+        }
+    };
+    D3D12_BLEND_DESC defaultBlendDesc() { return DEFAULT_D3D12_BLEND_DESC(); }
+};
