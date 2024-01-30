37968c8c4b9f95af600fa25e67a8d502421519c9
Bruno Evangelista <bpevangelista@gmail.com>
2024-01-30 10:14:29 -0800

    Update tiny sample to 100 lines

--- samples/tiny/tiny.cpp
+++ samples/tiny/tiny.cpp
@@ -1,40 +1,21 @@
 #include "../../fastdx/fastdx.h"
-#include <filesystem>
-#include <fstream>
 
 const int32_t kFrameCount = 3;
 const DXGI_FORMAT kFrameFormat = DXGI_FORMAT_R10G10B10A2_UNORM;
 const D3D12_CLEAR_VALUE kClearRenderTarget = { kFrameFormat, { 0.0f, 0.2f, 0.4f, 1.0f } };
-
+fastdx::WindowProperties windowProp;
 fastdx::D3D12DeviceWrapperPtr device;
 fastdx::ID3D12CommandQueuePtr commandQueue;
 fastdx::ID3D12CommandAllocatorPtr commandAllocators[kFrameCount];
 fastdx::ID3D12GraphicsCommandListPtr commandList;
 fastdx::IDXGISwapChainPtr swapChain;
 fastdx::ID3D12DescriptorHeapPtr swapChainRtvHeap;
-fastdx::ID3D12PipelineStatePtr pipelineState;
-fastdx::ID3D12RootSignaturePtr pipelineRootSignature;
 std::vector<fastdx::ID3D12ResourcePtr> renderTargets;
-std::vector<uint8_t> vertexShader, pixelShader;
 
 int32_t frameIndex = 0;
 HANDLE fenceEvent;
 fastdx::ID3D12FencePtr swapFence;
 uint64_t swapFenceCounter = 0, swapFenceWaitValue[kFrameCount] = {};
-
-HRESULT readShader(LPCWSTR filePath, std::vector<uint8_t>& outShaderData) {
-    WCHAR modulePathBuffer[1024];
-    GetModuleFileName(nullptr, modulePathBuffer, _countof(modulePathBuffer));
-    auto fullFilePath = std::filesystem::path(modulePathBuffer).parent_path() / filePath;
-
-    std::ifstream file(fullFilePath, std::ios::binary);
-    if (file) {
-        std::uintmax_t fileSize = std::filesystem::file_size(fullFilePath);
-        outShaderData.resize(fileSize);
-        file.read(reinterpret_cast<char*>(outShaderData.data()), fileSize);
-    }
-    return file? S_OK : E_FAIL;
-}
 
 void initializeD3d(HWND hwnd) {
     // Create a device and queue to dispatch command lists
@@ -42,9 +23,7 @@ void initializeD3d(HWND hwnd) {
     commandQueue = device->createCommandQueue(D3D12_COMMAND_LIST_TYPE_DIRECT);
 
     // Create a triple frame buffer swap chain for window
-    DXGI_SWAP_CHAIN_DESC1 swapChainDesc = fastdx::defaultSwapChainDesc(hwnd);
-    swapChainDesc.BufferCount = kFrameCount;
-    swapChainDesc.Format = kFrameFormat;
+    DXGI_SWAP_CHAIN_DESC1 swapChainDesc = fastdx::defaultSwapChainDesc(hwnd, kFrameCount, kFrameFormat);
     swapChain = device->createSwapChainForHwnd(commandQueue, swapChainDesc, hwnd);
 
     // Create a heap of descriptors, then them fill with swap chain render targets desc
@@ -63,20 +42,6 @@ void initializeD3d(HWND hwnd) {
     // Fence to wait for a completed frame to reuse
     swapFence = device->createFence(swapFenceCounter++, D3D12_FENCE_FLAG_NONE);
     fenceEvent = CreateEvent(nullptr, FALSE, FALSE, nullptr);
-
-    // Read VS and PS
-    readShader(L"simple_vs.cso", vertexShader);
-    readShader(L"simple_ps.cso", pixelShader);
-    
-    // Create a root signature for shaders
-    pipelineRootSignature = device->createRootSignature(0, vertexShader.data(), vertexShader.size());
-
-    // Create a pipeline state
-    D3D12_GRAPHICS_PIPELINE_STATE_DESC pipelineDesc = fastdx::defaultGraphicsPipelineDesc(kFrameFormat);
-    pipelineDesc.pRootSignature = pipelineRootSignature.get();
-    pipelineDesc.VS = { vertexShader.data(), vertexShader.size() };
-    pipelineDesc.PS = { pixelShader.data(), pixelShader.size() };
-    pipelineState = device->createGraphicsPipelineState(pipelineDesc);
 }
 
 void waitGpu(bool forceWait = false) {
@@ -98,13 +63,12 @@ void draw() {
     static size_t heapDescriptorSize = device->d3dDevice()->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
     D3D12_CPU_DESCRIPTOR_HANDLE frameRtvHandle = { rtvHandle.ptr + frameIndex * heapDescriptorSize };
 
-    static D3D12_RESOURCE_BARRIER transitionBarrier = { D3D12_RESOURCE_BARRIER_TYPE_TRANSITION, D3D12_RESOURCE_BARRIER_FLAG_NONE,
-        nullptr,  D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES };
+    static D3D12_RESOURCE_BARRIER transitionBarrier = { D3D12_RESOURCE_BARRIER_TYPE_TRANSITION,
+        D3D12_RESOURCE_BARRIER_FLAG_NONE, nullptr, D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES };
 
     // Get and reset allocator for current frame, then point command list to it
-    auto commandAllocator = commandAllocators[frameIndex];
-    commandAllocator->Reset();
-    commandList->Reset(commandAllocator.get(), nullptr);
+    commandAllocators[frameIndex]->Reset();
+    commandList->Reset(commandAllocators[frameIndex].get(), nullptr);
     {
         // Present->RenderTarget barrier
         transitionBarrier.Transition.pResource = renderTargets[frameIndex].get();
@@ -112,9 +76,9 @@ void draw() {
         transitionBarrier.Transition.StateAfter = D3D12_RESOURCE_STATE_RENDER_TARGET;
         commandList->ResourceBarrier(1, &transitionBarrier);
 
-        commandList->SetPipelineState(pipelineState.get());
         commandList->OMSetRenderTargets(1, &frameRtvHandle, FALSE, nullptr);
         commandList->ClearRenderTargetView(frameRtvHandle, kClearRenderTarget.Color, 0, nullptr);
+        // HERE --> Set GraphicsPipeline, SetRootSignature, Draw Things
 
         // RenderTarget->Present barrier
         transitionBarrier.Transition.StateBefore = D3D12_RESOURCE_STATE_RENDER_TARGET;
@@ -127,16 +91,12 @@ void draw() {
     ID3D12CommandList* commandLists[] = { commandList.get() };
     commandQueue->ExecuteCommandLists(_countof(commandLists), commandLists);
     swapChain->Present(1, 0);
-
     waitGpu();
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    fastdx::WindowProperties prop;
-    HWND hwnd = fastdx::createWindow(prop);
-    fastdx::onWindowDestroy = []() {
-        waitGpu(true);
-    };
+    HWND hwnd = fastdx::createWindow(windowProp);
+    fastdx::onWindowDestroy = []() { waitGpu(true); };
     initializeD3d(hwnd);
 
     return fastdx::runMainLoop(nullptr, draw);
