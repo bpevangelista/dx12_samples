0b410daa044d732aed4e9b6b0db539521c9ec917
Bruno Evangelista <bpevangelista@gmail.com>
2024-01-30 10:13:39 -0800

    Remove tensor core main

--- samples/_assets/simple_vs.hlsl
+++ samples/_assets/simple_vs.hlsl
@@ -14,7 +14,7 @@ StructuredBuffer<a2v> vertexBuffer : register(t0);
 [RootSignature("SRV(t0)")]
 v2f main(uint vid : SV_VertexID) {
     a2v IN = vertexBuffer[vid];
-    
+
     v2f OUT;
     OUT.position = IN.position;
     OUT.color = IN.color;
